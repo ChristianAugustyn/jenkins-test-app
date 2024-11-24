f9b141661c7190fdb729fea93c206f70b8295269
ChristianAugustyn <chrisaug@rogers.com>
2024-11-24 11:57:46 -0500

    added response object

--- src/main/java/com/augusch/jenkins_test_app/TestController.java
+++ src/main/java/com/augusch/jenkins_test_app/TestController.java
@@ -2,11 +2,8 @@ package com.augusch.jenkins_test_app;
 
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-
 
 @RestController
 @RequestMapping(produces="application/json")
